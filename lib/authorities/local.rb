db9e5ff4ad19874e45e1fb59c0b2a550c152916d
Jim Coble <jim.coble@duke.edu>
2013-09-17 16:13:48 -0400

    add authorities config and term queries

--- lib/authorities/local.rb
+++ lib/authorities/local.rb
@@ -4,11 +4,14 @@ module Authorities
     
     attr_accessor :response
     
-    def initialize(q, authority)
-      self.response = []
-      terms = YAML.load(File.read(File.join(Rails.root, 'config', 'authorities', "#{authority}.yml")))
+    def initialize(q, sub_authority)
+      terms = YAML.load(File.read(File.join(Rails.root, AUTHORITIES_CONFIG[:local_path], "#{sub_authority}.yml")))[:terms]
       if q.blank?
         self.response = terms
+      else
+        sub_terms = []
+        terms.each { |term| sub_terms << term if term[:label].start_with?(q) }
+        self.response = sub_terms
       end
     end
 
@@ -16,6 +19,12 @@ module Authorities
       self.response.to_json
     end
     
+    def sub_authorities
+      sub_auths = []
+      Dir.foreach(File.join(Rails.root, AUTHORITIES_CONFIG[:local_path])) { |file| sub_auths << File.basename(file, File.extname(file)) }
+      sub_auths
+    end
+        
   end
   
 end
--- spec/lib/authorities_local_spec.rb
+++ spec/lib/authorities_local_spec.rb
@@ -2,11 +2,25 @@ require 'spec_helper'
 
 describe Authorities::Local do
 
-  context "retrieve all entries for a local authority" do
+  context "retrieve all entries for a local sub_authority" do
+    
+    it "should validate the sub_authority" do
+      Authorities::Local.new("", "school").sub_authorities.should include "school"
+    end
     
     it "should return all the entries" do
       terms = Authorities::Local.new("", "school")
       terms.results.should include "School of Business"
+    end
+    
+  end
+  
+  context "retrieve a subset of entries for a local sub_authority" do
+    
+    it "should return only entries matching the query term" do
+      terms = Authorities::Local.new("School", "school")
+      terms.results.should include "School of Business"
+      terms.results.should_not include "College of Arts and Sciences"
     end
     
   end
