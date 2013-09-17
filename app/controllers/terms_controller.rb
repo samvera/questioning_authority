9bc30a2e8503d8627a5d96469f38473061855d22
Mike Stroming <mstrom81@gmail.com>
2013-09-17 13:38:26 -0500

    Incremental commit. Changing terms_controller to handle more than one vocab

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,7 +1,27 @@
 class TermsController < ApplicationController
-
+  
   def index
-    @results = Authorities::Lcsh.new(params[:q]) if params[:q]
+    
+    #these are the supported vocabularies and the associated class names
+    vocabularies = {"lcsh"=>"Authorities::Lcsh", "loc"=>"Authorities::loc"}
+    
+    #make sure vocab param is present
+    if !params[:vocab].present?
+      raise Exception, 'The vocabulary was not specified'
+    end
+    
+    #make sure q param is present
+    if !params[:q].present?
+      raise Exception, 'The query was not specified'
+    end
+    
+    #make sure vocab param is valid
+    if !vocabularies.has_key? params[:vocab]
+      raise Exception, 'Vocabulary not supported'
+    end
+    
+    #use the appropriate class (get the name from the hash map) and retrieve the vocabulary
+    @results = vocabularies[params[:vocab]].constantize.new(params[:q])
 
     respond_to do |format|
       format.html { render :layout => false, :text => @results.to_json }
@@ -9,5 +29,6 @@ class TermsController < ApplicationController
       format.js   { render :layout => false, :text => @results.to_json }
     end
   end
+
 
 end
