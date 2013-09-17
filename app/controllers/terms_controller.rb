c5ca766403ca610de2fbde8f72da07f57696580a
Mike Stroming <mstrom81@gmail.com>
2013-09-17 15:51:00 -0500

    Variable name changes

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -3,7 +3,7 @@ class TermsController < ApplicationController
   def index
     
     #these are the supported vocabularies and the associated class names
-    vocabularies = {"lcsh"=>"Authorities::Lcsh", "loc"=>"Authorities::Loc"}
+    authorities_classes = {"lcsh"=>"Authorities::Lcsh", "loc"=>"Authorities::Loc"}
     
     #make sure vocab param is present
     if !params[:vocab].present?
@@ -16,24 +16,30 @@ class TermsController < ApplicationController
     end
     
     #make sure vocab param is valid
-    if !vocabularies.has_key? params[:vocab]
+    if !authorities_classes.has_key? params[:vocab]
       raise Exception, 'Vocabulary not supported'
     end
     
+    #get the authority class
+    authority_class = authorities_classes[params[:vocab]]
+    
     #convert wildcard to be URI encoded
     params[:q].gsub!("*", "%2A")
-
-    #use the appropriate class (get the name from the hash map) and retrieve the vocabulary
-    if params[:sub_authority].present?
-      @results = vocabularies[params[:vocab]].constantize.new(params[:q], params[:sub_authority])
+   
+   #initialize the authority and run the search. if there's a sub-authority and it's valid, include that param
+    if params[:sub_authority].present? and authority_class.constantize.authority_valid?(params[:sub_authority])
+      @authority = authority_class.constantize.new(params[:q], params[:sub_authority])
     else
-      @results = vocabularies[params[:vocab]].constantize.new(params[:q])
+      @authority = authority_class.constantize.new(params[:q])
     end
     
+    #parse the results
+    @authority.parse_authority_response
+    
     respond_to do |format|
-      format.html { render :layout => false, :text => @results.to_json }
-      format.json { render :layout => false, :text => @results.to_json }
-      format.js   { render :layout => false, :text => @results.to_json }
+      format.html { render :layout => false, :text => @authority.results }
+      format.json { render :layout => false, :text => @authority.results }
+      format.js   { render :layout => false, :text => @authority.results }
     end
   end
 
