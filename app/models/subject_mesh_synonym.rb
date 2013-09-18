188d3777d709c36f2cad394cb62dc4f352b0db1e
Don Brower <dbrower@nd.edu>
2013-09-18 14:31:52 -0400

    remove unneeded database model

--- app/models/subject_mesh_synonym.rb
+++
@@ -1,4 +0,0 @@
-class SubjectMeshSynonym < ActiveRecord::Base
-  belongs_to :subject_mesh_entry , :foreign_key => "subject_mesh_term_id"
-  attr_accessible :subject_synonym, :subject_mesh_term_id
-end
