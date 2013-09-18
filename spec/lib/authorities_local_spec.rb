09f41d6d8ddf39637a3bacfe79e44d4282f36039
Jim Coble <jim.coble@duke.edu>
2013-09-17 21:29:06 -0400

    improve tests

--- spec/lib/authorities_local_spec.rb
+++ spec/lib/authorities_local_spec.rb
@@ -2,15 +2,24 @@ require 'spec_helper'
 
 describe Authorities::Local do
 
-  context "retrieve all entries for a local sub_authority" do
+  before do
+    AUTHORITIES_CONFIG[:local_path] = File.join('spec', 'fixtures', 'authorities')
+  end
+  
+  context "valid local sub_authorities" do
     
     it "should validate the sub_authority" do
-      Authorities::Local.sub_authorities.should include "school"
+      Authorities::Local.sub_authorities.should include "authority_A"
+      Authorities::Local.sub_authorities.should include "authority_B"
     end
     
+  end
+
+  context "retrieve all entries for a local sub_authority" do
+    
     it "should return all the entries" do
-      authorities = Authorities::Local.new("", "school")
-      authorities.parse_authority_response.should include "School of Business"
+      authorities = Authorities::Local.new("", "authority_A")
+      authorities.parse_authority_response.should include "Term A2"
     end
     
   end
@@ -18,9 +27,9 @@ describe Authorities::Local do
   context "retrieve a subset of entries for a local sub_authority" do
     
     it "should return only entries matching the query term" do
-      authorities = Authorities::Local.new("School", "school")
-      authorities.parse_authority_response.should include "School of Business"
-      authorities.parse_authority_response.should_not include "College of Arts and Sciences"
+      authorities = Authorities::Local.new("Abc", "authority_A")
+      authorities.parse_authority_response.should include "Abc Term A1"
+      authorities.parse_authority_response.should_not include "Term A2"
     end
     
   end
