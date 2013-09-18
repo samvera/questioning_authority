85a6c646bd175b58ab741d63391e6705e7d3075d
Michael Durbin <md5wz@d-137-155-231.bootp.Virginia.EDU>
2013-09-18 11:14:00 -0400

    Updated SRU authorities implementation to parameterize the URL. Added more sub_authorities.

--- lib/authorities/sru.rb
+++ lib/authorities/sru.rb
@@ -7,20 +7,20 @@ module Authorities
   
     SRU_SERVER_CONFIG = YAML.load_file(Rails.root.join("config", "sru-authorities.yml"))
     
-    attr_accessor :response, :query_url
+    attr_accessor :response, :query_url, :raw_response
 
     def initialize(q, sub_authority='')
-      self.query_url = SRU_SERVER_CONFIG["authorities"][sub_authority]["url"].gsub(/\{query\}/, q)
-      sru_xml_response = Nokogiri::XML(open(self.query_url))
-      self.response = parse_authority_response(sru_xml_response)
+      self.query_url = SRU_SERVER_CONFIG["url-pattern"]["prefix-query"].gsub(/\{query\}/, q).gsub(/\{authority\-id\}/, sub_authority)
+      self.raw_response = Nokogiri::XML(open(self.query_url))
+      self.response = parse_authority_response(self.raw_response)
     end
 
-    def valid?(sub_authority)
+    def self.authority_valid?(sub_authority)
       sub_authorities.include?(sub_authority)
     end
 
-    def sub_authorities
-      a = Array.new
+    def self.sub_authorities
+      a = []
       SRU_SERVER_CONFIG["authorities"].each do | sub_authority |
         a.append(sub_authority[0])
       end
@@ -33,6 +33,20 @@ module Authorities
         r.append({"id" => record.xpath('Zthes/term/termId').first.content, "label" => record.xpath('Zthes/term/termName').first.content})
       end
       r
+    end
+
+    def get_full_record(id, sub_authority)
+      if !self.raw_response.xpath("sru:searchRetrieveResponse/sru:records/sru:record/sru:recordData/Zthes/term[termId='" + id + "']", 'sru' => 'http://www.loc.gov/zing/srw/').nil?
+        parse_full_record(raw_xml, id)
+      else
+        parse_full_record(Nokogiri::XML(open(SRU_SERVER_CONFIG["url-pattern"]["id-lookup-query"].gsub(/\{id\}/, id).gsub(/\{authority\-id\}/, sub_authority))))
+      end
+      
+    end
+    
+    def parse_full_record(raw_xml, id)
+        zthes_record = raw_xml.xpath("sru:searchRetrieveResponse/sru:records/sru:record/sru:recordData/Zthes/term[termId='" + id + "']");
+        
     end
 
     def results
