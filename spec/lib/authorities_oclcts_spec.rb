8d52233eab456ff4015034a81faa95ea7a910bbd
stormfin <rdfloyd@localhost.localdomain>
2013-09-18 19:06:33 +0200

    Refactoring test to mock net call with webmock

--- spec/lib/authorities_oclcts_spec.rb
+++ spec/lib/authorities_oclcts_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe Authorities::Oclcts do
 
   before :all do
+    WebMock.disable_net_connect!
+    # TODO: Need a more elegant way to stub the request when the URL string is crazy long
+    stub_request(:get, "http://tspilot.oclc.org/mesh/?maximumRecords=10&operation=searchRetrieve&query=oclcts.rootHeading%20exact%20%22ball*%22&recordPacking=xml&recordSchema=http://zthes.z3950.org/xml/1.0/&recordXPath=&resultSetTTL=300&sortKeys=&startRecord=1&version=1.1").
+    # TODO: Where should response text files go?
+    to_return(:body => File.new(Rails.root.join("spec/fixtures", "oclcts-response.txt")), :status => 200)
+
     @terms = Authorities::Oclcts.new("ball", "mesh").parse_authority_response
   end
 
