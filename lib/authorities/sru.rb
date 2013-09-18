1f19ce1696ea79b7037dac1e6baef7d9d1e918b9
Michael Durbin <md5wz@d-137-155-231.bootp.Virginia.EDU>
2013-09-18 09:41:34 -0400

    Updated SRU authority.

--- lib/authorities/sru.rb
+++ lib/authorities/sru.rb
@@ -4,19 +4,39 @@ require 'nokogiri'
 module Authorities
 
   class SRU
-    attr_accessor :response
+  
+    SRU_SERVER_CONFIG = YAML.load_file(Rails.root.join("config", "sru-authorities.yml"))
+    
+    attr_accessor :response, :query_url
 
-    def initialize q
-      sru_xml_response = Nokogiri::XML(open("http://tspilot.oclc.org/mesh/?query=oclcts.rootHeading+exact+%22" + q + "*%22&version=1.1&operation=searchRetrieve&recordSchema=http%3A%2F%2Fzthes.z3950.org%2Fxml%2F1.0%2F&maximumRecords=10&startRecord=1&resultSetTTL=300&recordPacking=xml&recordXPath=&sortKeys="))
+    def initialize(q, sub_authority='')
+      self.query_url = SRU_SERVER_CONFIG["authorities"][sub_authority]["url"].gsub(/\{query\}/, q)
+      sru_xml_response = Nokogiri::XML(open(self.query_url))
+      self.response = parse_authority_response(sru_xml_response)
+    end
+
+    def valid?(sub_authority)
+      sub_authorities.include?(sub_authority)
+    end
+
+    def sub_authorities
+      a = Array.new
+      SRU_SERVER_CONFIG["authorities"].each do | sub_authority |
+        a.append(sub_authority[0])
+      end
+      a
+    end
+
+    def parse_authority_response(raw_response)
       r = Array.new
-      sru_xml_response.xpath('sru:searchRetrieveResponse/sru:records/sru:record/sru:recordData', 'sru' => 'http://www.loc.gov/zing/srw/').each do |record|
+      raw_response.xpath('sru:searchRetrieveResponse/sru:records/sru:record/sru:recordData', 'sru' => 'http://www.loc.gov/zing/srw/').each do |record|
         r.append({"id" => record.xpath('Zthes/term/termId').first.content, "label" => record.xpath('Zthes/term/termName').first.content})
       end
-      self.response = r.to_json
+      r
     end
-    
+
     def results
-      self.response[1].to_json
+      self.response.to_json
     end
   end
 end
--- spec/lib/authorities_sru_spec.rb
+++ spec/lib/authorities_sru_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe Authorities::SRU do
 
   before :all do
-    @terms = Authorities::SRU.new "ball"
-    @parsed = JSON.parse(@terms.response)
+    @terms = Authorities::SRU.new("ball", "mesh").response
   end
 
   # TODO: These test the reponse from SRU server and should be moved to
@@ -12,15 +11,15 @@ describe Authorities::SRU do
   describe "the response from SRU" do
 
     it "should have an array of hashes that match the query" do
-      @parsed.should be_kind_of Array
-      @parsed.first.should be_kind_of Hash
-      @parsed.first["label"].should be_kind_of String
-      @parsed.first["label"].should include "Ballota"
+      @terms.should be_kind_of Array
+      @terms.first.should be_kind_of Hash
+      @terms.first["label"].should be_kind_of String
+      @terms.first["label"].should include "Ballota"
     end
 
     it "should have an array of hashes containing unique id and label" do
-      @parsed.first.should have_key("id")
-      @parsed.first.should have_key("label")
+      @terms.first.should have_key("id")
+      @terms.first.should have_key("label")
     end
 
   end
