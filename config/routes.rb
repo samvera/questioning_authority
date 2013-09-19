27be674149aaf7481d0c545cc80083025672d7ee
Mike Stroming <mstrom81@gmail.com>
2013-09-19 10:17:30 -0500

    Fixed syntax issue with search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 QuestioningAuthority::Application.routes.draw do
-  match "search/:vocab" => "terms#search", :via=>:get
-  match "search/:vocab/:sub_authority" => "terms#search", :via=>:get
+  match "/search/:vocab" => "terms#search", :via=>:get
+  match "/search/:vocab/:sub_authority" => "terms#search", :via=>:get
   match "/terms/:vocab" => "terms#index", :via=>:get
   match "/terms/:vocab/:sub_authority" => "terms#index", :via=>:get
   resources :terms
