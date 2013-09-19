7ff7b9f63188f132c62d795f848b544dfe0c5187
Mike Stroming <mstrom81@gmail.com>
2013-09-19 10:03:10 -0500

    Changed routes, controller refactor for index and search actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 QuestioningAuthority::Application.routes.draw do
+  match "search/:vocab" => "terms#search", :via=>:get
+  match "search/:vocab/:sub_authority" => "terms#search", :via=>:get
+  match "/terms/:vocab" => "terms#index", :via=>:get
+  match "/terms/:vocab/:sub_authority" => "terms#index", :via=>:get
   resources :terms
 end
