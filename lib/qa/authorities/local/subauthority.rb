fc369b4def97431c850bead89fa6a1d974dfd669
Justin Coyne <justin@curationexperts.com>
2013-11-06 16:17:10 -0600

    Factor out local sub-authority

---
+++ lib/qa/authorities/local/subauthority.rb
@@ -0,0 +1,54 @@
+module Qa::Authorities
+  # This is really just a sub-authority for Local
+  class Subauthority
+    attr_reader :name
+    def initialize(name)
+      @name = name
+    end
+
+    def terms
+      @terms ||= load_sub_authority_terms
+    end
+
+    class << self
+      def sub_authorities_path
+        config_path = AUTHORITIES_CONFIG[:local_path]
+        if config_path.starts_with?(File::Separator)
+          return config_path
+        end
+        File.join(Rails.root, config_path)
+      end
+
+      def names
+        @sub_auth_names ||=
+          begin
+            sub_auths = []
+            Dir.foreach(sub_authorities_path) { |file| sub_auths << File.basename(file, File.extname(file)) }
+            sub_auths
+          end
+      end
+    end
+
+    private
+      def load_sub_authority_terms
+        sub_authority_hash = YAML.load(File.read(sub_authority_filename))
+        terms = sub_authority_hash.with_indifferent_access.fetch(:terms, [])
+        normalize_terms(terms)
+      end
+
+      def sub_authority_filename
+        File.join(self.class.sub_authorities_path, "#{name}.yml")
+      end
+
+      def normalize_terms(terms)
+        terms.map do |term|
+          if term.is_a? String
+            { :id => term, :term => term }.with_indifferent_access
+          else
+            term[:id] ||= term[:term]
+            term
+          end
+        end
+      end
+  end
+end
