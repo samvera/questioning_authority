aa5ea657973ee3a74f77f0aef82dfa97bf017f6a
Justin Coyne <justin@curationexperts.com>
2013-11-06 14:39:01 -0600

    Raise errors in YAML parsing when they are encountered

--- lib/qa/authorities/local.rb
+++ lib/qa/authorities/local.rb
@@ -4,44 +4,51 @@ module Qa::Authorities
 
     attr_accessor :response
 
-    def self.sub_authorities_path
-      config_path = AUTHORITIES_CONFIG[:local_path]
-      if config_path.starts_with?(File::Separator)
-        return config_path
+    class << self
+
+      def sub_authorities_path
+        config_path = AUTHORITIES_CONFIG[:local_path]
+        if config_path.starts_with?(File::Separator)
+          return config_path
+        end
+        File.join(Rails.root, config_path)
       end
-      File.join(Rails.root, config_path)
-    end
 
-    def self.sub_authorities
-      @sub_auths ||=
-        begin
-          sub_auths = []
-          Dir.foreach(self.sub_authorities_path) { |file| sub_auths << File.basename(file, File.extname(file)) }
-          sub_auths
-        end
-    end
-
-    def self.terms(sub_authority)
-      @terms = {} if @terms.nil?
-      return @terms[sub_authority] if @terms.has_key?(sub_authority)
-      sub_authority_hash =
-        begin
-          YAML.load(File.read(File.join(self.sub_authorities_path, "#{sub_authority}.yml")))
-        rescue
-          {}
-        end
-      @terms[sub_authority] = normalize_terms(sub_authority_hash.fetch(:terms, []))
-    end
-
-    def self.normalize_terms(terms)
-      terms.map do |term|
-        if term.is_a? String
-          { :id => term, :term => term }
-        else
-          term[:id] ||= term[:term]
-          term
-        end
+      def sub_authorities
+        @sub_auths ||=
+          begin
+            sub_auths = []
+            Dir.foreach(sub_authorities_path) { |file| sub_auths << File.basename(file, File.extname(file)) }
+            sub_auths
+          end
       end
+
+      def terms(sub_authority)
+        @terms = {} if @terms.nil?
+        return @terms[sub_authority] if @terms.has_key?(sub_authority)
+        load_sub_authority_terms(sub_authority)
+      end
+
+      private
+        def load_sub_authority_terms(sub_authority)
+          sub_authority_hash = YAML.load(File.read(sub_authority_filename(sub_authority)))
+          @terms[sub_authority] = normalize_terms(sub_authority_hash.fetch(:terms, []))
+        end
+
+        def sub_authority_filename(sub_authority)
+          File.join(sub_authorities_path, "#{sub_authority}.yml")
+        end
+
+        def normalize_terms(terms)
+          terms.map do |term|
+            if term.is_a? String
+              { :id => term, :term => term }
+            else
+              term[:id] ||= term[:term]
+              term
+            end
+          end
+        end
     end
 
     def initialize
