92b79fb4c66880f45844c4bd6a57dcf1a63832d8
Justin Coyne <justin@curationexperts.com>
2015-04-09 15:20:10 -0500

    Add the ability to register new local vocabularies

--- lib/qa/authorities/local.rb
+++ lib/qa/authorities/local.rb
@@ -4,13 +4,83 @@ module Qa::Authorities
     extend Qa::Authorities::LocalSubauthority
     require 'qa/authorities/local/file_based_authority'
 
+
     def self.factory(sub_authority)
       validate_sub_authority!(sub_authority)
-      FileBasedAuthority.new(sub_authority)
+      registry.instance_for(sub_authority)
+    end
+
+    def self.registry
+      @registry ||= begin
+        Registry.new do |reg|
+          register_defaults(reg)
+        end
+      end
+    end
+
+
+    def self.register_factory(sub_authority, class_name)
+      registry.add(sub_authority, class_name)
     end
 
     def self.sub_authorities
-      names
+      registry.keys
+    end
+
+    private
+      def self.register_defaults(reg)
+        names.each do |name|
+          reg.add(name, 'Qa::Authorities::Local::FileBasedAuthority')
+        end
+      end
+
+    class Registry
+      def initialize
+        @hash = {}
+        yield self if block_given?
+      end
+
+      def keys
+        @hash.keys
+      end
+
+      def instance_for(key)
+        fetch(key).instance
+      end
+
+      def fetch(key)
+        @hash.fetch(key)
+      end
+
+      def self.logger
+        @logger ||= begin
+          ::Rails.logger if defined? Rails and Rails.respond_to? :logger
+        end
+      end
+
+      def self.logger= logger
+        @logger = logger
+      end
+
+      def add(sub_authority, class_name)
+        Registry.logger.debug "Registering Local QA authority: #{sub_authority} - #{class_name}"
+        @hash[sub_authority] = RegistryEntry.new(sub_authority, class_name)
+      end
+
+
+      class RegistryEntry
+        def initialize(sub_authority, class_name)
+          @sub_authority, @class_name = sub_authority, class_name
+        end
+
+        def klass
+          @class_name.constantize
+        end
+
+        def instance
+          klass.new(@sub_authority)
+        end
+      end
     end
   end
 end
--- spec/lib/authorities/local_spec.rb
+++ spec/lib/authorities/local_spec.rb
@@ -4,25 +4,45 @@ describe Qa::Authorities::Local do
 
   describe "new" do
     it "should raise an error" do
-      expect { Qa::Authorities::Local.new }.to raise_error
+      expect { described_class.new }.to raise_error
     end
   end
 
   describe ".factory" do
     context "without a sub-authority" do
       it "should raise an error is the sub-authority is not provided" do
-        expect { Qa::Authorities::Local.factory }.to raise_error
+        expect { described_class.factory }.to raise_error
       end
       it "should raise an error is the sub-authority does not exist" do
-        expect { Qa::Authorities::Local.factory("foo") }.to raise_error
+        expect { described_class.factory("foo") }.to raise_error
       end
     end
 
     context "with a sub authority" do
-      subject { Qa::Authorities::Local.factory("authority_A") }
+      subject { described_class.factory("authority_A") }
       it "should return a file authority" do
         expect(subject).to be_kind_of Qa::Authorities::Local::FileBasedAuthority
       end
+    end
+  end
+
+  describe ".register" do
+    before do
+      class SolrAuthority
+        def initialize(one)
+        end
+      end
+      described_class.register_factory('new_sub', 'SolrAuthority')
+    end
+
+    after { Object.send(:remove_const, :SolrAuthority) }
+
+    it "adds an entry to sub_authorities" do
+      expect(described_class.sub_authorities).to include 'new_sub'
+    end
+
+    it "creates authorities of the proper type" do
+      expect(described_class.factory('new_sub')).to be_kind_of SolrAuthority
     end
   end
 end
