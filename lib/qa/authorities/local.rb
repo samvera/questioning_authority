ef06a99cffd33508d1e2ce022ab5c489d97a186c
Justin Coyne <jcoyne@justincoyne.com>
2016-06-29 09:22:29 -0500

    Merge Authorities::LocalSubauthority and Authorities::Local
    `LocalSubauthority` was never used by any other module, so it was
    unnecessary to have it separate.

--- lib/qa/authorities/local.rb
+++ lib/qa/authorities/local.rb
@@ -2,9 +2,26 @@ module Qa::Authorities
   module Local
     extend ActiveSupport::Autoload
     extend AuthorityWithSubAuthority
-    extend Qa::Authorities::LocalSubauthority
     autoload :FileBasedAuthority
     autoload :Registry
+
+    # Path to sub-authority files is either the full path to a directory or
+    # the path to a directory relative to the Rails application
+    def self.subauthorities_path
+      if AUTHORITIES_CONFIG[:local_path].starts_with?(File::Separator)
+        AUTHORITIES_CONFIG[:local_path]
+      else
+        File.join(Rails.root, AUTHORITIES_CONFIG[:local_path])
+      end
+    end
+
+    # Local sub-authorities are any YAML files in the subauthorities_path
+    def self.names
+      unless Dir.exists? subauthorities_path
+        raise Qa::ConfigDirectoryNotFound, "There's no directory at #{subauthorities_path}. You must create it in order to use local authorities"
+      end
+      Dir.entries(subauthorities_path).map { |f| File.basename(f, ".yml") if f.match(/yml$/) }.compact
+    end
 
     def self.subauthority_for(subauthority)
       validate_subauthority!(subauthority)
