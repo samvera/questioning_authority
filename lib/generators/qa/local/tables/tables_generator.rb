feae19da50d8c47ac7ed89d3c33ce7d20a9b953c
Carolyn Cole <cam156@psu.edu>
2016-07-14 10:13:30 -0400

    Changing the generator to link to the name spaced table.

--- lib/generators/qa/local/tables/tables_generator.rb
+++ lib/generators/qa/local/tables/tables_generator.rb
@@ -7,6 +7,13 @@ module Qa::Local
     def migrations
       generate "model qa/local_authority name:string:uniq"
       generate "model qa/local_authority_entry local_authority:references label:string uri:string:uniq"
+      migration_file = Dir.entries(File.join(destination_root,'db/migrate/'))
+                           .reject{|name| !name.include?('create_qa_local_authority_entries')}.first
+      migration_file = File.join('db/migrate',migration_file)
+      gsub_file migration_file,
+                't.references :local_authority, index: true, foreign_key: true',
+                't.references :local_authority, foreign_key: { to_table: :qa_local_authorities }, index: true'
+
       message = "Rails doesn't support functional indexes in migrations, so you'll have to add this manually:\n" \
     "CREATE INDEX \"index_qa_local_authority_entries_on_lower_label\" ON \"qa_local_authority_entries\" (local_authority_id, lower(label))\n" \
     "   OR on Sqlite: \n" \
