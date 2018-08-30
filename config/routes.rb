7dbf2cb084c24bbed75d8334ae609c0c6859e447
E. Lynette Rayle <elr37@cornell.edu>
2018-08-30 16:54:48 -0400

    add support for CORS headers
    * Added CORS support as a configurable option that can be enable or disabled.  It is disabled by default to maintain backward compatibility.
    * When CORS is enabled...
      * Adds CORS response 'Access-Control-Allow-Origin' for all GET requests
      * Handles OPTIONS method requests for supported routes and responds with supported methods and allow origin header
    * When CORS is disabled...
      * The 'Access-Control-Allow-Origin' header is not added to GET requests
      * If OPTIONS method is requested, a 501 (not supported) response is returned
    
    REF: https://fetch.spec.whatwg.org/#http-cors-protocol

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,12 @@ Qa::Engine.routes.draw do
   get "/search/:vocab(/:subauthority)", controller: :terms, action: :search
   get "/show/:vocab/:id", controller: :terms, action: :show
   get "/show/:vocab/:subauthority/:id", controller: :terms, action: :show
+
+  match "/search/linked_data/:vocab(/:subauthority)", to: 'application#options', via: [:options]
+  match "/show/linked_data/:vocab/:id", to: 'application#options', via: [:options]
+  match "/show/linked_data/:vocab/:subauthority/:id", to: 'application#options', via: [:options]
+  match "/terms/:vocab(/:subauthority)",  to: 'application#options', via: [:options]
+  match "/search/:vocab(/:subauthority)", to: 'application#options', via: [:options]
+  match "/show/:vocab/:id", to: 'application#options', via: [:options]
+  match "/show/:vocab/:subauthority/:id", to: 'application#options', via: [:options]
 end
