c3cf23c9a492a234a5f37be0643ed4dec201dec4
E. Lynette Rayle <elr37@cornell.edu>
2021-11-04 10:39:13 -0400

    add fetch action allowing for uri parameter

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Qa::Engine.routes.draw do
   get "/search/:vocab(/:subauthority)", controller: :terms, action: :search
   get "/show/:vocab/:id", controller: :terms, action: :show
   get "/show/:vocab/:subauthority/:id", controller: :terms, action: :show
+  get "/fetch/:vocab", controller: :terms, action: :fetch
+  get "/fetch/:vocab/:subauthority", controller: :terms, action: :fetch
 
   match "/search/linked_data/:vocab(/:subauthority)", to: 'application#options', via: [:options]
   match "/show/linked_data/:vocab/:id", to: 'application#options', via: [:options]
@@ -17,4 +19,6 @@ Qa::Engine.routes.draw do
   match "/search/:vocab(/:subauthority)", to: 'application#options', via: [:options]
   match "/show/:vocab/:id", to: 'application#options', via: [:options]
   match "/show/:vocab/:subauthority/:id", to: 'application#options', via: [:options]
+  match "/fetch/:vocab", to: 'application#options', via: [:options]
+  match "/fetch/:vocab/:subauthority", to: 'application#options', via: [:options]
 end
