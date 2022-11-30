80a3f1d3f8b3e93dd53dd1dcb85184fe6c52562b
Jonathan Rochkind <jonathan@dnil.net>
2022-11-30 14:01:42 -0500

    Move Qa::LinkedData::AuthorityService to lib/
    This class is accessed in an *initializer* (see config/initializers/linked_data_authorities.rb:). In Rails 7, this is somehow a loading problem, it results in:
    
    ```
    questioning_authority/config/initializers/linked_data_authorities.rb:1:in `<top (required)>': uninitialized constant Qa::LinkedData (NameError)
    	from /Users/jrochkind/.gem/ruby/3.0.4/gems/railties-7.0.4/lib/rails/engine.rb:667:in `load'
    ```
    
    Moving this class from app/ to lib/, and explicitly requiring it at boot in the engine.rb file, seems to resolve the problem and allow tests to pass on Rails 7. See also #351

--- lib/qa/engine.rb
+++ lib/qa/engine.rb
@@ -1,3 +1,6 @@
+# used in initializer where autoload is unavailable, must be required here:
+require 'qa/linked_data/authority_service'
+
 module Qa
   class Engine < ::Rails::Engine
     isolate_namespace Qa
