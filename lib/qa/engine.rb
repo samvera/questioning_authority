0c3877e6d1ac9fb8c5ab32cf97478cee378d985c
Jonathan Rochkind <jonathan@dnil.net>
2023-11-08 11:30:27 -0500

    Add workaround for engine_cart-generated generator conflicting with Rails 7.1 zeitwerk default config.
    https://github.com/cbeer/engine_cart/issues/117

--- lib/qa/engine.rb
+++ lib/qa/engine.rb
@@ -4,5 +4,18 @@ require 'qa/linked_data/authority_service'
 module Qa
   class Engine < ::Rails::Engine
     isolate_namespace Qa
+
+    config.before_configuration do
+      # rubocop:disable Style/IfUnlessModifier
+
+      # see https://github.com/fxn/zeitwerk#for_gem
+      # Blacklight puts a generator into LOCAL APP lib/generators, so tell
+      # zeitwerk to ignore the whole directory? If we're using zeitwerk
+      #
+      # See: https://github.com/cbeer/engine_cart/issues/117
+      if Rails.try(:autoloaders).try(:main).respond_to?(:ignore)
+        Rails.autoloaders.main.ignore(Rails.root.join('lib', 'generators'))
+      end
+    end
   end
 end
