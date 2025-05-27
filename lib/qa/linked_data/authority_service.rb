133e1a958af8a3b25b8815f9c1b5a2872ab9c164
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-05-27 14:28:19 -0400

    make authority_service load a new config file to govern the behavior of assign_fast authorities

--- lib/qa/linked_data/authority_service.rb
+++ lib/qa/linked_data/authority_service.rb
@@ -4,23 +4,39 @@ module Qa
     class AuthorityService
       # Load or reload the linked data configuration files
       def self.load_authorities
-        auth_cfg = {}
-        # load QA configured linked data authorities
-        Dir[File.join(Qa::Engine.root, 'config', 'authorities', 'linked_data', '*.json')].each do |fn|
-          auth = File.basename(fn, '.json').upcase.to_sym
-          json = File.read(File.expand_path(fn, __FILE__))
-          cfg = JSON.parse(json).deep_symbolize_keys
-          auth_cfg[auth] = cfg
-        end
+        load_linked_data_config
+        load_assign_fast_config
+      end
 
-        # load app configured linked data authorities and overrides
-        Dir[Rails.root.join('config', 'authorities', 'linked_data', '*.json')].each do |fn|
-          auth = File.basename(fn, '.json').upcase.to_sym
-          json = File.read(File.expand_path(fn, __FILE__))
-          cfg = JSON.parse(json).deep_symbolize_keys
-          auth_cfg[auth] = cfg
+      def self.load_linked_data_config
+        ld_auth_cfg = {}
+        Dir[File.join(Qa::Engine.root, 'config', 'authorities', 'linked_data', '*.json')].each do |fn|
+          process_config_file(file_path: fn, config_hash: ld_auth_cfg)
         end
-        Qa.config.linked_data_authority_configs = auth_cfg
+        Dir[Rails.root.join('config', 'authorities', 'linked_data', '*.json')].each do |fn|
+          process_config_file(file_path: fn, config_hash: ld_auth_cfg)
+        end
+        Qa.config.linked_data_authority_configs = ld_auth_cfg
+      end
+
+      # similar to the above; these settings are for getting (non-linked-data) FAST subject headings from OCLC.
+      def self.load_assign_fast_config
+        assign_fast_auth_cfg = {}
+        Dir[File.join(Qa::Engine.root, 'config', 'authorities', 'assign_fast', '*.json')].each do |fn|
+          process_config_file(file_path: fn, config_hash: assign_fast_auth_cfg)
+        end
+        Dir[Rails.root.join('config', 'authorities', 'assign_fast', '*.json')].each do |fn|
+          process_config_file(file_path: fn, config_hash: assign_fast_auth_cfg)
+        end
+        Qa.config.assign_fast_authority_configs = assign_fast_auth_cfg
+      end
+
+      # load settings into a configuration array
+      def self.process_config_file(file_path:, config_hash:)
+        file_key = File.basename(file_path, '.json').upcase.to_sym
+        json = File.read(File.expand_path(file_path, __FILE__))
+        cfg = JSON.parse(json).deep_symbolize_keys
+        config_hash[file_key] = cfg
       end
 
       # Get the list of names of the loaded authorities
